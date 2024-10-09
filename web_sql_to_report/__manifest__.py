9443888f93885bafe14ec87fe69fc415af4fb865
liong999 <nagara987@gmail.com>
2024-10-09 22:47:07 +0700

    ADD : Cover and margin

--- web_sql_to_report/__manifest__.py
+++ web_sql_to_report/__manifest__.py
@@ -13,6 +13,7 @@
     'author': "Liong",
     'website': "https://www.linkedin.com/in/nagara-liong-50ab07136/",
     "license": "AGPL-3",
+    'images': ['static/description/cover.png'],
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
